bccd50b599fdbb4e6c2fd4db46bd797b811220e8
Bruno Elario Rheinheimer <brunoelario@gmail.com>
2021-12-17 09:24:51 -0300

    primeiro commit

---
+++ src/test/java/com/Integracaocomapi/IntegrandoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Integracaocomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrandoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
